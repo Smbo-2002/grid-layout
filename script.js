63962155792472292c5d585a00243a574efcf0eb
Smbat <smbo.voskanyan2002@gmail.com>
2019-06-27 16:10:02 +0400

    Responsive grid layout
    Responsive grid layout

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (function ($) {
 	// Default options for plugin
 	var defaultOpts = {
-		width: 360,
-		padding: 15
+		width: 180,
+		padding: 20
 	}
 	
 	// Defining the plugin
@@ -10,14 +10,55 @@
 		var options = $.extend({}, defaultOpts, options);
 		
 		elementInit(options, this);
+
+		$(window).resize(function(){   
+			wrap(options, this);
+		});
 	};
 
 	// Initializing given element
 	function elementInit (options, element) {
 		$(element).addClass('grid-layout');
+		var viewWidth = $('.grid-layout').width();
+		var columns = Math.floor(viewWidth / options.width);
+		var currColumn = 1;
 		$(element).children().each(function (index, item) {
-			var html = "<img width='"+ options.width +"px' alt='"+ $(item).attr('data-alt') +"' src='"+ $(item).attr('data-url') +"' />"
-			$(item).append(html)
+			var left = 0;
+			var top = 0;
+			var html = "<img width='"+ options.width +"px' alt='"+ $(item).attr('data-alt') +"' src='"+ $(item).attr('data-url') +"'/>";
+			$(item).append(html);
+			left = (currColumn-1)*(options.width + options.padding);
+			if(currColumn+1 > columns) {
+				currColumn = 1;
+			} else {
+				currColumn++;
+			}
+			if (index+1>columns) {
+				top = parseInt($($(element).children()[index-columns]).css('top').replace('px','')) + parseInt($($(element).children()[index-columns]).css('height').replace('px',''));
+			}
+			$(item).css("left", left + "px" );
+			$(item).css("top", top + options.padding + "px" );
+		});
+	}
+
+	function wrap (options, element) {
+		var viewWidth = $('.grid-layout').width();
+		var columns = Math.floor(viewWidth / options.width);
+		var currColumn = 1;
+		$('.grid-layout').children().each(function (index, item) {
+			var left = 0;
+			var top = 0;
+			left = (currColumn-1)*(options.width + options.padding);
+			if(currColumn+1 > columns) {
+				currColumn = 1;
+			} else {
+				currColumn++;
+			}
+			if (index+1>columns) {
+				top = parseInt($($('.grid-layout').children()[index-columns]).css('top').replace('px','')) + parseInt($($('.grid-layout').children()[index-columns]).css('height').replace('px',''));
+			}
+			$(item).css("left", left + "px" );
+			$(item).css("top", top + options.padding + "px" );
 		});
 	}
 
